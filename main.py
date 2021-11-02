440ffe416378d34af7a89033d20fcda830349e25
Oleg Rudakov <nefoser@gmail.com>
2021-11-02 20:56:53 +0300

    Add function is_bitlink

--- main.py
+++ main.py
@@ -7,34 +7,30 @@ load_dotenv()
 TOKEN = os.environ.get("BitlyToken")
 REQUEST_URL = "https://api-ssl.bitly.com/v4/bitlinks"
 INPUT_URL = input("Enter your link: ")
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}"
+}
 
 
 def shorten_link(TOKEN, INPUT_URL):
     body = {
         "long_url": INPUT_URL
     }
-    headers = {
-        "Authorization": f"Bearer {TOKEN}"
-    }
-
-    response = requests.post(REQUEST_URL, headers=headers, json=body)
+    response = requests.post(REQUEST_URL, headers=HEADERS, json=body)
     response.raise_for_status()
 
-    bitlink = json.loads(response.text)["link"]
+    bitlink = json.loads(response.text)["id"]
 
     return bitlink
 
 
 def count_clicks(TOKEN, INPUT_URL):
     count_url = f"{REQUEST_URL}/{INPUT_URL}/clicks/summary"
-    headers = {
-        "Authorization": f"Bearer {TOKEN}"
-    }
     params = {
         "unit": "day",
         "units": -1
     }
-    response = requests.get(count_url, headers=headers, params=params)
+    response = requests.get(count_url, headers=HEADERS, params=params)
     response.raise_for_status()
 
     clicks_count = json.loads(response.text)["total_clicks"]
@@ -42,12 +38,18 @@ def count_clicks(TOKEN, INPUT_URL):
     return clicks_count
 
 
-try:
-    clicks_count = count_clicks(TOKEN, INPUT_URL)
-    print("Clicks", clicks_count)
-    #bitlink = shorten_link(TOKEN, INPUT_URL)
-    #print('Your bitlink', bitlink)
-except requests.exceptions.HTTPError:
-    print("ERROR")
+def is_bitlink(INPUT_URL):
+    if "bit.ly" in INPUT_URL:
+        try:
+            clicks_count = count_clicks(TOKEN, INPUT_URL)
+            print("Clicks", clicks_count)
+        except requests.exceptions.HTTPError:
+            print("ERROR")
+    else:
+        try:
+            bitlink = shorten_link(TOKEN, INPUT_URL)
+            print('Your bitlink:', bitlink)
+        except requests.exceptions.HTTPError:
+            print("ERROR")
 
-#print(count_clicks(TOKEN, INPUT_URL))
+is_bitlink(INPUT_URL)
