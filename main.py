d725ddecda6c87b7ad083e2d8eebd07524f263b4
Oleg Rudakov <nefoser@gmail.com>
2021-11-02 18:39:04 +0300

    Add post request for get bitlink

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 
 load_dotenv()
 TOKEN = os.environ.get("BitlyToken")
+LONG_URL = os.environ.get("LongURL")
+
+body = {
+    "long_url": LONG_URL
+}
 headers = {
     "Authorization": f"Bearer {TOKEN}"
 }
-url = "https://api-ssl.bitly.com/v4/user"
-response = requests.get(url, headers=headers)
+
+url = "https://api-ssl.bitly.com/v4/bitlinks"
+response = requests.post(url, headers=headers, json=body)
 response.raise_for_status()
 
-print(response.text)
+date_response = json.loads(response.text)
+
+print(date_response["link"])
